71f7f838bcdb574af5176819f50e89c7fb87e711
bibian8 <bibicami06@gmail.com>
2025-05-16 09:54:13 -0500

    segundo commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { View,Text, StyleSheet, TextInput, Image, TouchableOpacity } from "react-native";
+import { View,Text, StyleSheet, TextInput, Image, TouchableOpacity, Dimensions, FlatList } from "react-native";
 
 const estilos=StyleSheet.create({
   contenedor:{
     width:'100%',
-    height:500,
+
     padding:40,
-  /*   backgroundColor:'blue' */
+    
   },
   texto:{
     fontSize:50,
@@ -16,35 +16,83 @@ const estilos=StyleSheet.create({
     borderColor:'black',
     borderWidth:1.5,
     borderRadius:15,
-    marginTop:20
+    flex:1
   },
   emoji:{
     width:50,
     height:50
   },
   boton:{
-    width:80,
-    height:30,
-    padding:3,
+    width:Dimensions.get("screen").width*0.25,
     backgroundColor:'green',
     borderColor:'black',
     borderWidth:1.5,
-    borderRadius:10
+    borderRadius:10,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  Ctareas:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    gap:10,
+    marginTop:10
+  },
+  dataTasks:{
+    paddingVertical:20,
+    borderBottomColor:'#6f6f6f',
+    borderBottomWidth:5
+  },
+  letras:{
+    fontSize:20,
+    color:'black'
   }
 })
+const tasks=[{
+  titulo:"Estudiar",
+  done:false,
+  date:new Date()
+},
+{
+  titulo:"Jugar",
+  done:false,
+  date:new Date()
+},
+{
+  titulo:"Caminar",
+  done:false,
+  date:new Date()
+}
+]
+interface Task{
+  titulo:string,
+  done:boolean,
+  date:Date
+}
+function renderItem({item}:{item:Task}){
+  return ( <View style={estilos.dataTasks}>
+    <Text style={estilos.letras}>{item.titulo}</Text>
+  </View>
+  )}
 export default function App(){
   return(
     <View style={estilos.contenedor}>
       <Text style={estilos.texto}>Mis tareas</Text>
-      <View >
+      <View style={estilos.Ctareas}>
         <TextInput placeholder="Escriba" style={estilos.input}/>
         <TouchableOpacity style={estilos.boton}>
-          <Text>
+          <Text style={estilos.letras}>
             Agregar
           </Text>
         </TouchableOpacity>
+      </View>
+      <View>
+        <FlatList
+        renderItem={renderItem}
+        data={tasks}
+        />
       </View>
       <Image source={require('./img/emoji.jpg')} style={estilos.emoji}/>
     </View>
   )
 }
+  
